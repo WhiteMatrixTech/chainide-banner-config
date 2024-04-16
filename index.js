6d50350c44aac8a66fe3620e3863748e5606c45f
Hexi <66669483+Hexi1997@users.noreply.github.com>
2024-04-16 03:52:24 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export const bannerConfig = {
-    link: `https://beta.chainide.com/events/`,
+    link: `https://chainide.com/events/`,
     pcImg: `/imgs/pc.jpg`,
     mobileImg: `/imgs/mobile.jpg`
 }
